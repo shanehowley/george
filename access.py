857bcd55df8820da99cb3279b599d82318f6a52c
shanehowley <snhowley@gmail.com>
2017-02-20 16:49:29 +0000

    inital commit

--- access.py
+++ access.py
@@ -13,10 +13,16 @@ auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 count = 10
-query = ' trump'
+query = 'Dublin'
 
 
 results = [status for status in tweepy.Cursor(api.search, q=query).items(count)]
+print json.dumps(results[0]._json, indent=4)
 
-for result in results:
-    print json.dumps(result._json, indent=2)
+for status in results:
+    print status.text.encode('utf-8')
+    print status.user.id
+    print status.user.screen_name
+    print status.user.profile_image_url_https
+    print status.user.followers_count
+    print status.place
